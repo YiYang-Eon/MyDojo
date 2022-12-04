9969f1455f391ddec48f471b36e12b917a9c9ecf
YiYang-Eon <15594999221@163.com>
2022-12-04 14:35:32 +0800

    feat: add sort function

--- MyJavaDojo/src/main/java/com/echo/mashibing/suanfa/sort/BubbleSort.java
+++ MyJavaDojo/src/main/java/com/echo/mashibing/suanfa/sort/BubbleSort.java
@@ -2,24 +2,24 @@ package com.echo.mashibing.suanfa.sort;
 
 import java.util.Arrays;
 
-public class SelectSort {
+public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 3, 5, 2, 4, 99, 7, 9, 3, 1, 6, 11, 3, 45};
-        selectSort(arr);
+        bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void selectSort(int[] arr) {
+    private static void bubbleSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
         int N = arr.length;
-        for (int i = 0; i < N; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < N; j++) {
-                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+        for (int end = N - 1; end >= 0; end--) {
+            for (int second = 1; second <= end; second++) {
+                if (arr[second - 1] > arr[second]) {
+                    swap(arr, second - 1, second);
+                }
             }
-            swap(arr, i, minIndex);
         }
     }
 
--- MyJavaDojo/src/main/java/com/echo/mashibing/suanfa/sort/InsertSort.java
+++ MyJavaDojo/src/main/java/com/echo/mashibing/suanfa/sort/InsertSort.java
@@ -2,24 +2,24 @@ package com.echo.mashibing.suanfa.sort;
 
 import java.util.Arrays;
 
-public class SelectSort {
+public class InsertSort {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 3, 5, 2, 4, 99, 7, 9, 3, 1, 6, 11, 3, 45};
-        selectSort(arr);
+        insertSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void selectSort(int[] arr) {
+    private static void insertSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
         int N = arr.length;
-        for (int i = 0; i < N; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < N; j++) {
-                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+        for (int end = 1; end < N; end++) {
+            int newIndex = end;
+            while (newIndex - 1 >= 0 && arr[newIndex - 1] > arr[newIndex]) {
+                swap(arr, newIndex - 1, newIndex);
+                newIndex--;
             }
-            swap(arr, i, minIndex);
         }
     }
 
@@ -29,4 +29,3 @@ public class SelectSort {
         arr[i] = temp;
     }
 }
-
