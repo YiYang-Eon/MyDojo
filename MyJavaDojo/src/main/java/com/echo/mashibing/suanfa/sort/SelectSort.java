20b06d8ab717efc12988d26db5700bb0b0870267
YiYang-Eon <15594999221@163.com>
2022-12-04 14:05:52 +0800

    feat: first commit

---
+++ MyJavaDojo/src/main/java/com/echo/mashibing/suanfa/sort/SelectSort.java
@@ -0,0 +1,31 @@
+package com.echo.mashibing.suanfa.sort;
+
+import java.util.Arrays;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 5, 2, 4, 99, 7, 9, 3, 1, 6, 11, 3, 45};
+        selectSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void selectSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i ,minIndex);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
+
